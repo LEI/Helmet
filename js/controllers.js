f598d827ead77cb777ad9155abe133be7f01d0bc
Guillaume Frichet <LEI@lei.home>
2014-05-20 23:28:59 +0200

    position

--- js/controllers.js
+++ js/controllers.js
@@ -23,39 +23,56 @@ angular.module('appHelmet.controllers', [
 
 	if (navigator.geolocation) {
 
-		geolocation.getCurrentPosition(
-			// Success
-			function(position) {
-				console.log(position);
-				$scope.test += '<strong>getCurrentPosition</strong><br>' +
-					'Latitude: ' + position.coords.latitude + '<br>' +
-					'Longitude: ' + position.coords.longitude + '<br>';
-			},
-			// Fail
-			function(error) {
-				console.log(error);
-				$scope.test += '<strong>getCurrentPosition</strong><br>' +
-				'Error code ' + error.code + ' - ' + error.message + '<br>';
-			},
-			{ timeout: 3000, enableHighAccuracy: true, maximumAge: 0 }
-		);
-
+		// Watch position
 		geolocation.watchPosition(
 			// Success
 			function(position) {
+				$scope.position = position;
 				console.log(position);
-				$scope.test += '<strong>watchPosition</strong><br>' +
+				$scope.test += '<strong>Position changed</strong> (watchPosition)<br>' +
 					'Latitude: ' + position.coords.latitude + '<br>' +
 					'Longitude: ' + position.coords.longitude + '<br>';
 			},
 			// Fail
 			function(error) {
 				console.log(error);
-				$scope.test += '<strong>watchPosition</strong><br>' +
-				'Error code ' + error.code + ' - ' + error.message + '<br>';
+				$scope.test += '<strong>Error code ' + error.code + '</strong> (watchPosition)<br>' + error.message + '<br>';
 			},
-			{ timeout: 5000, enableHighAccuracy: true, maximumAge: 0 }
+			{
+				timeout: 1000,
+				enableHighAccuracy: true,
+				maximumAge: 0
+			}
 		);
+
+		var intervalId = setInterval(function () {
+			// Get current position
+			geolocation.getCurrentPosition(
+				// Success
+				function(position) {
+					$scope.position = position;
+					console.log(position);
+					$scope.test += '<strong>Current position</strong> (getCurrentPosition)<br>' +
+						'Latitude: ' + position.coords.latitude + '<br>' +
+						'Longitude: ' + position.coords.longitude + '<br>';
+				},
+				// Fail
+				function(error) {
+					console.log(error);
+					$scope.test += '<strong>Error code ' + error.code + '</strong> (getCurrentPosition)<br>' + error.message + '<br>';
+				},
+				{
+					timeout: 1000,
+					enableHighAccuracy: true,
+					maximumAge: 0
+				}
+			);
+		}, 1000);
+
+		$scope.$on('$destroy', function () {
+			clearInterval(intervalId);
+		});
+
 	} else {
 		alert('FU');
 	}
