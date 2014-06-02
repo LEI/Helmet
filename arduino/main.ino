70ef4a5073fcbab74672d70533105d0ff68a6f75
paulguzda <paulguzdariviere@gmail.com>
2014-06-02 15:37:31 +0200

    serial communication (tests)

--- arduino/main.ino
+++ arduino/main.ino
@@ -1,7 +1,31 @@
+#include <SoftwareSerial.h>
+
 int led = 13;
-int digit_rx = 0,
-	digit_tx = 1,
-	rn52cmd = 2;
+int rx = 0,
+    tx = 1,
+    
+    rn52_rx = 5,
+    rn52_tx = 6,
+
+    rn52cmd = 2,
+    rn52evtreg = 4;
+
+SoftwareSerial SerialRN52( rn52_rx, rn52_tx ); // RX, TX
+
+/********* FUNCTIONS **********/
+
+/**
+ * Enter or leave RN-52 Command Mode
+ */
+int rn52_commandmode( bool on = false ) {
+	if(on) {
+		digitalWrite( rn52cmd, LOW );
+	} else {
+		digitalWrite( rn52cmd, HIGH );
+	}
+}
+
+/********* MAIN **********/
 
 void setup() {
 	pinMode(led, OUTPUT);
@@ -9,16 +33,31 @@ void setup() {
 	/**
 	 * Configure pins
 	 */
-	pinMode(digit_rx, INPUT); // Main data receiving
-	pinMode(digit_tx, OUTPUT); // Main data transmitting
- 	
- 	pinMode(rn52cmd, INPUT);
+ 	pinMode( rn52cmd, OUTPUT );   // RN-52 Command Mode
+ 	pinMode( rn52evtreg, INPUT ); // RN-52 Event Register
+
+ 	/**
+	 * Command monitoring (returns)
+	 */
+	 Serial.begin( 115200 );
+         SerialRN52.begin( 115200 );
 }
 
 void loop() {
 	// Basic programm (blink)
-	digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+	digitalWrite( led, HIGH );   // turn the LED on (HIGH is the voltage level)
 	delay(1000);               // wait for a second
-	digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+	digitalWrite( led, LOW );    // turn the LED off by making the voltage LOW
 	delay(1000);               // wait for a second
+
+	// Main programm
+//	rn52_commandmode( true );
+        digitalWrite( rn52cmd, LOW );
+
+        Serial.print( SerialRN52.read() );
+       
+//	rn52_commandmode( false );
+        digitalWrite( rn52cmd, HIGH );
+
+        Serial.print( SerialRN52.read() );
 }
