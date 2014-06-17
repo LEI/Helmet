652c8d3cddccdf38c6d3368375bdad444e4a4426
Guillaume Frichet <LEI@LEI.local>
2014-06-17 15:26:13 +0200

    LCD

--- arduino/mega/mega.ino
+++ arduino/mega/mega.ino
@@ -1,12 +1,15 @@
+#include <LiquidCrystal.h>
 #define BUFFER_SIZE 64
 
 int cmdPin = 2,
-		evtPin = 4,
-		bpsPin = 3,
+    evtPin = 4,
+    bpsPin = 3,
     resetPin = 5;
+    
+LiquidCrystal lcd(12, 11, 10, 8, 9 ,7);
 
 void setup() {
-
+        
 	pinMode(evtPin,INPUT);
 	pinMode(resetPin,INPUT);
 
@@ -26,36 +29,49 @@ void setup() {
 
 	Serial.println("serial bluetooth started");
 
-        pinMode(cmdPin, OUTPUT);
-        digitalWrite(cmdPin, LOW);
-
+        //cmdMode();
+        lcd.begin(20,2);
 }
 
-unsigned char buf[BUFFER_SIZE] = {0};
-unsigned char len = 0;
+unsigned char buf[BUFFER_SIZE] = {0}, str[BUFFER_SIZE] = {0};
+unsigned char len = 0, count = 0;
+unsigned char c;
 
 void loop() {
   
         // RN52 return
 	if (Serial1.available()) {
           while(Serial1.available() > 0) {
-	    Serial.write(Serial1.read());
+            c = Serial1.read();
+            Serial.write(c);
+            if (c != '\n' && c != '\r') {
+              lcd.write(c);
+            } else {
+              lcd.write(" ");
+            }
+            
+            
           }
 	}
 
         while(Serial.available() > 0) {
-          unsigned char c = Serial.read();
+          c = Serial.read();
           Serial.write(c);
           if(c == 0xA || c == 0xD) {
             sendData();
           } else {
             bufferData(c);
           }
-          if(c == '&') {
+          if (c == '&') {
             resetFactory();
-          } else if (c == 'X') {
+          } else if (c == '#') {
              Serial1.write("SF,1\r");
+             delay(20);
              Serial1.write("R,1\r");
+          } else if (c == '*') {
+            dataMode();
+          } else if (c == '$') {
+            cmdMode();
           }
         }
 }
@@ -69,12 +85,26 @@ void bufferData(char c) {
 }
 
 void sendData() {
+  lcd.clear();
   for(byte i = 0; i < len; i++) {
     Serial1.write(buf[i]);
   }
   Serial1.write(0xD);
   len = 0;
   Serial1.flush();
+}
+
+void cmdMode() {
+  Serial.println("[CMD MODE]");
+  pinMode(cmdPin, OUTPUT);
+  digitalWrite(cmdPin, LOW);
+  delay(100);
+}
+
+void dataMode() {
+  Serial.println("[DATA MODE]");
+  pinMode(cmdPin, INPUT);
+  delay(100);
 }
 
 void resetFactory() {
@@ -92,5 +122,5 @@ void resetFactory() {
   pinMode(resetPin, INPUT);
   delay(1000);
   Serial1.write('\r');
-  Serial.println("-> OK");
+  Serial.println(" -> OK");
 }
