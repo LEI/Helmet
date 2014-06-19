9ac7746800d325c05cf72363960ff5d0829b26cf
Guillaume Frichet <LEI@LEI.local>
2014-06-19 12:15:23 +0200

    fs

--- www/js/controllers/trips.js
+++ www/js/controllers/trips.js
@@ -13,31 +13,17 @@ function($q, $scope, $rootScope, $timeout, $localStorage, FileSystem) {
 
 	$rootScope.$storage = $localStorage;
 
-<<<<<<< HEAD
-=======
 	FileSystem.write({speed:2});
 	FileSystem.read();
-	
+
 	//FileSystem.write("{data:[]}");
 
-	// read, JSON.parse
-
-	// var.push(obj)
-	
-	// JSON.stringify
-
-	// save
-
-	// FileSystem.write("{lol:1}");
-
-/*
->>>>>>> 6ea8ed965a560ed4cfbf345dc1bc717e62fcbc68
 	FileSystem.read().then(function(response){
 		console.log(response);
 		$scope.tripList = response;
 	}, function(error){
 		console.log(error);
 	});
-*/
+
 
 }]);
