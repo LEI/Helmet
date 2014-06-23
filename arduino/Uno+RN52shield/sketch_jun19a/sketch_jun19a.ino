6e370ed6d0cd6769da6b3f0d639cacc368b37be0
Paul GR <paulguzdariviere@gmail.com>
2014-06-23 13:50:49 +0200

    update code

---
+++ arduino/Uno+RN52shield/sketch_jun19a/sketch_jun19a.ino
@@ -0,0 +1,15 @@
+//int cmdPin = 2;
+
+void setup() {
+//        pinMode(cmdPin, OUTPUT);
+//        digitalWrite(cmdPin, LOW);
+        
+//        pinMode(cmdPin, INPUT);
+}
+
+
+void loop() {
+  
+// Serial.read();
+        
+}
